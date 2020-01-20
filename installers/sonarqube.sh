3b993e984666e44f2ac896176190bfcfacad284a
lootfold <pallav.92@gmail.com>
2020-01-20 21:50:20 +0530

    adds cmd to install sonar-scanner

--- installers/sonarqube.sh
+++ installers/sonarqube.sh
@@ -14,3 +14,12 @@ cd ~/installers
 unzip sonarqube-7.9.2.zip
 
 mv sonarqube-7.9.2 ~/apps/sonarqube
+
+# install sonar scanner
+wget 'https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.2.0.1873-linux.zip' -P ~/installers
+
+unzip sonar-scanner-cli-4.2.0.1873-linux.zip
+
+mv sonar-scanner-cli-4.2.0.1873-linux ~/apps/sonar-scanner
+
+sudo ln -s ln -s ~/apps/sonar-scanner/bin/sonar-scanner /usr/bin/sonar-scanner
